2be1e35bb0ddf01e74ad29471d64514252033bc6
Austin Kohler <austinckohler@gmail.com>
2020-03-26 02:52:03 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -16,4 +16,4 @@ end
 # pp directors_database
 # puts print_first_directors_movie_titles
 # puts directors_database[0][:movies]
-puts directors_database[0][:movies][5][:title]
+puts directors_database[0][:movies][6][:title]
